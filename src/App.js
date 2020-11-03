86273dfb7954002d626efd29ce9e69196be7eef4
Ryan Schniederjan <ryno@schnietke.com>
2020-11-03 06:51:51 -0600

    Added Auth0 Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    
+      <div className="App">
+        
+      </div>
+    
   );
 }
 

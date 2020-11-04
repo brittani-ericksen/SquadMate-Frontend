90ffb4d04da82b775c94e07c265f88fc316e4ab8
Annemarie Thomas <Athomas9sa@users.noreply.github.com>
2020-11-04 16:25:33 -0500

    Styled: Home Layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./components/Home/Home";
 function App() {
     return (
         <div className='App'>
             <Router>
-                <Layout />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/login">
+                        <Layout />
+                    </Route>
+                </Switch>        
             </Router>
+            
         </div>
     );
 }
--- src/components/Home/LoginButton.jsx
+++ src/components/Home/LoginButton.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from '@material-ui/core/Button';
 
-const LoginButton = () => {
+const Button = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <LoginButton onClick={() => loginWithRedirect()}>Log In</LoginButton>;
 };
 
-export default LoginButton;
+export default Button;

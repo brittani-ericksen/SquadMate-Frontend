78a38614fc34bd3461a4bd01f60c9b1a98c92c7d
Ryan Schniederjan <ryno@schnietke.com>
2020-11-05 11:41:56 -0600

    Added a local user

---
+++ localData.js
@@ -0,0 +1,47 @@
+const localUser =
+{
+  parentForm: {
+      rider: {
+          firstName: "Joe",
+          lastName: "Shlomo"
+        },
+      parentOne: {
+          phone: {
+              home: "713-234-9876",
+              cell: "806-345-5641"
+              },
+          firstName: "Bill",
+          lastName: "Shlomo"
+      },
+      parentTwo: {
+          phone: {
+              home: "713-678-1256",
+              cell: "806-356-4896"
+              },
+          firstName: "Jill",
+          lastName: "Shlomo"
+      }
+  },
+  insurance: {
+      provider: "Cigna",
+      group: "W7854RT5985-000",
+      number: "0000008578592652"
+      },
+  isAdmin: false,
+  medicalConditions: false,
+  asthma: false,
+  medicationRequired: false,
+  ibprofenRelease: false,
+  firstName: "Joe",
+  lastName: "Shlomo",
+  userName: "rider",
+  email: "co@me.com",
+  password: "password",
+  team: {
+      $oid: "5fa2dd0998fe8fbfdf5eaac2"
+      },
+  allergies: "No Allergies",
+  __v: 0,
+}
+
+export default localUser;

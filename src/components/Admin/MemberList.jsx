42caf1c58a8ceb39cfb874412b2b5c7961d9938e
Ryan Schniederjan <ryno@schnietke.com>
2020-11-06 16:08:37 -0600

    Memberlist Grid

--- src/components/Admin/MemberList.jsx
+++ src/components/Admin/MemberList.jsx
@@ -1,45 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardActions, CardActionArea, CardMedia, CardContent, Button, Typography } from '@material-ui/core';
+import { Card, CardActions, CardActionArea, CardMedia, CardContent, Button, Typography, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles({
-    root: {
+    gridRoot: {
+        flexGrow:1,
+        justifyContent: "center",
+    },
+    cardRoot: {
         maxWidth: 345,
     },
-    media: {
-        height: 140,
+    image: {
+        height: "auto",
+        width: "50%",
+        alignItems: "flex-end",
+
     },
 });
 
 const MemberList = () =>{
     const classes = useStyles();
+    const[teamData, setTeamData] = useState([]);
+
+    useEffect(() => {
+        let teamId = '5fa2dd0998fe8fbfdf5eaac2';
+
+        (async function() {
+            const response = await fetch(`http://localhost:3333/team/${teamId}/users`);
+            const data = await response.json();
+            setTeamData(data);
+        })();
+    },[setTeamData])
+    
 
     return (
         <>
-            {/* card components - display one for each member */}
-            <Card className={classes.root}>
-                <CardActionArea>
-                    <CardMedia
-                        className={classes.media}
-                        image="../../public/avatar-placeholder.png"
-                        title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            Name
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            **** Additional info ****
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" color="primary">
-                        View Profile
-                        </Button>
-                    </CardActions>
-                </CardActionArea>
-            </Card>
-        </>
+        <div className={classes.gridRoot}>
+        <Grid container justify='center'
+        spacing={3}
+        >
+        {teamData.map((mate) =>(
+            <>
+                    <Grid item xs={12}>
+                        <Card className={classes.cardRoot}>
+                            <CardMedia
+                                className={classes.image}
+                                component="img"
+                                image="/avatar-placeholder.png"
+                                title={mate.firstName}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {mate.firstName} {mate.lastName}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    **** Additional info ****
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                View Profile
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>    
+                
+                <p></p>
+            </>
+        ))}
+        </Grid>
+        </div>
+    </>
     );
 }
 

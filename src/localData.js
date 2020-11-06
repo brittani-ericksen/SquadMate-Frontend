c8b465feeac0f7ca24f78ec937978b69388aa3b9
Chris Owens <guitarrockstarco@gmail.com>
2020-11-06 14:17:16 -0500

    Update routes for signin and signup and update user functionality

--- src/localData.js
+++ src/localData.js
@@ -1,51 +1,50 @@
-const localUser =
-{
-  parentForm: {
-      rider: {
-          firstName: "Joe",
-          lastName: "Shlomo"
+const localUser = {
+    parentForm: {
+        rider: {
+            firstName: 'Joe',
+            lastName: 'Shlomo',
         },
-      parentOne: {
-          phone: {
-              home: "713-234-9876",
-              cell: "806-345-5641"
-              },
-          firstName: "Bill",
-          lastName: "Shlomo"
-      },
-      parentTwo: {
-          phone: {
-              home: "713-678-1256",
-              cell: "806-356-4896"
-              },
-          firstName: "Jill",
-          lastName: "Shlomo"
-      }
-  },
-  insurance: {
-      provider: "Cigna",
-      group: "W7854RT5985-000",
-      number: "0000008578592652"
-      },
-  isAdmin: false,
-  medicalConditions: false,
-  asthma: false,
-  medicationRequired: false,
-  ibprofenRelease: false,
-  firstName: "Joe",
-  lastName: "Shlomo",
-  userName: "rider",
-  email: "co@me.com",
-  password: "password",
-  phone: {
-      cellphone: '555-555-5555',
-      home: '555-555-5555'
-  },
-  team: {
-      $oid: "5fa2dd0998fe8fbfdf5eaac2"
-      },
-  allergies: "No Allergies",
-  __v: 0,
-}
+        parentOne: {
+            phone: {
+                home: '713-234-9876',
+                cell: '806-345-5641',
+            },
+            firstName: 'Bill',
+            lastName: 'Shlomo',
+        },
+        parentTwo: {
+            phone: {
+                home: '713-678-1256',
+                cell: '806-356-4896',
+            },
+            firstName: 'Jill',
+            lastName: 'Shlomo',
+        },
+    },
+    insurance: {
+        provider: 'Cigna',
+        group: 'W7854RT5985-000',
+        number: '0000008578592652',
+    },
+    isAdmin: true,
+    medicalConditions: false,
+    asthma: false,
+    medicationRequired: false,
+    ibprofenRelease: false,
+    firstName: 'Joe',
+    lastName: 'Shlomo',
+    userName: 'rider',
+    email: 'co@me.com',
+    password: 'password',
+    phone: {
+        cell: '555-555-5555',
+        home: '555-555-5555',
+    },
+    team: {
+        $oid: '5fa2dd0998fe8fbfdf5eaac2',
+    },
+    allergies: 'No Allergies',
+    __v: 0,
+};
 
 export default localUser;

1be3a2d64e6bcf490a38c31de914c81c7feb990e
Chris Owens <guitarrockstarco@gmail.com>
2020-11-12 10:45:12 -0500

    Add document component to display forms

--- src/components/User/UserDocuments.jsx
+++ src/components/User/UserDocuments.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Container, Button } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 

a76e6d379cee32d61373add5276de631e2c852b6
Chris Owens <guitarrockstarco@gmail.com>
2020-11-11 21:38:34 -0500

    Add profile pic and document upload

---
+++ src/components/Admin/SingleDocumentItem.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import styled from 'styled-components';
+
+const Document = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 10px;
+`;
+
+const Button = styled.div`
+    border: none;
+    color: red;
+    border-radius: 50px;
+    cursor: pointer;
+`;
+
+const SingleDocumentItem = ({document, user, setDocuments, documents}) => {
+    console.log(document._id);
+    const handleDelete = async () => {
+        const response = await fetch(
+            `http://localhost:3333/document/${document._id}`,
+            {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+            }
+        );
+        const resdata = await response.json();
+        console.log(resdata);
+        setDocuments(resdata);
+
+    }
+    return (
+        <Document>     
+            <div>           
+            <a href={document.url} rel="noreferrer" target="_blank">
+                {!!document.formName ? document.formName: 'Form'}
+            </a>
+            </div>
+            <Button onClick={handleDelete}><HighlightOffIcon /></Button>
+        </Document>
+    )
+}
+
+export default SingleDocumentItem

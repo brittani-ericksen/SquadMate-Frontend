a61663f526757115f593c085533ba20616af7d63
Brittani Ericksen <heybrittani@gmail.com>
2020-11-13 13:05:35 -0500

    finished breadcrumbs

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -4,12 +4,26 @@ import AdminForms from "./AdminForms";
 import AdminProfile from "./AdminProfile";
 import TeamList from "./TeamList";
 import { makeStyles } from '@material-ui/core/styles';
+import { Breadcrumbs } from '@material-ui/core';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import HomeIcon from '@material-ui/icons/Home';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import GroupIcon from '@material-ui/icons/Group';
+import PersonIcon from '@material-ui/icons/Person';
+import RecentActorsIcon from '@material-ui/icons/RecentActors';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 import Avatar from 'react-avatar';
 import { Switch, Route, Link } from 'react-router-dom';
 import RiderCard from './RiderCard';
+import styled from 'styled-components';
 
+const BreadcrumbWrapper = styled.div`
+    background-color: #FFBA00;
+    width: 350px;
+    border-radius: 0 7px 7px 0;
+`;
 
 const images = [
 {
@@ -39,87 +53,96 @@ const images = [
 ];
 
 const useStyles = makeStyles((theme) => ({
-root: {
-    color: '#E6EAF3',
-    display: 'flex',
-    flexWrap: 'wrap',
-    minWidth: 300,
-    width: '100%',
-},
-picButton: {
-    dipslay: 'block'
-},
-image: {
-    position: 'relative',
-    height: 200,
-    [theme.breakpoints.down('xs')]: {
-    width: '100% !important', // Overrides inline-style
-    height: 100,
+    root: {
+        color: '#E6EAF3',
+        display: 'flex',
+        flexWrap: 'wrap',
+        minWidth: 300,
+        width: '100%',
     },
-    '&:hover, &$focusVisible': {
-    zIndex: 1,
-    '& $imageBackdrop': {
-        opacity: 0.15,
+    picButton: {
+        dipslay: 'block'
     },
-    '& $imageMarked': {
-        opacity: 0,
+    image: {
+        position: 'relative',
+        height: 200,
+        [theme.breakpoints.down('xs')]: {
+            width: '100% !important', // Overrides inline-style
+            height: 100,
+        },
+        '&:hover, &$focusVisible': {
+            zndex: 1,
+            '& $imageBackdrop': {
+                opacity: 0.15,
+            },
+            '& $imageMarked': {
+                opacity: 0,
+            },
+            '& $imageTitle': {
+                border: '4px solid currentColor',
+            },
+        },
     },
-    '& $imageTitle': {
-        border: '4px solid currentColor',
+    link: {
+        display: 'flex',
+        alignItems: 'center',
     },
+    icon: {
+        marginRight: "5px",
+        width: 20,
+        height: 20,
     },
-},
-focusVisible: {},
-imageButton: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.palette.common.white,
-},
-imageSrc: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    margin: '5px 10px',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center 40%',
-},
-imageBackdrop: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    margin: '5px 10px',
-    backgroundColor: theme.palette.common.black,
-    opacity: 0.4,
-    transition: theme.transitions.create('opacity'),
-},
-imageTitle: {
-    position: 'relative',
-    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
-},
-imageMarked: {
-    height: 3,
-    width: 18,
-    backgroundColor: theme.palette.common.white,
-    position: 'absolute',
-    bottom: -2,
-    left: 'calc(50% - 9px)',
-    transition: theme.transitions.create('opacity'),
-},
-upload: {
-    backgroundColor: '#E6EAF3',
-    borderRadius: '30px',
-    width: '210px'
-}
+    focusVisible: {},
+    imageButton: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: theme.palette.common.white,
+    },
+    imageSrc: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        margin: '5px 10px',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center 40%',
+    },
+    imageBackdrop: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        margin: '5px 10px',
+        backgroundColor: theme.palette.common.black,
+        opacity: 0.4,
+        transition: theme.transitions.create('opacity'),
+    },
+    imageTitle: {
+        position: 'relative',
+        padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
+    },
+    imageMarked: {
+        height: 3,
+        width: 18,
+        backgroundColor: theme.palette.common.white,
+        position: 'absolute',
+        bottom: -2,
+        left: 'calc(50% - 9px)',
+        transition: theme.transitions.create('opacity'),
+    },
+    upload: {
+        backgroundColor: '#E6EAF3',
+        borderRadius: '30px',
+        width: '210px'
+    }
 }));
 
 
@@ -137,8 +160,6 @@ const Admin = (props) =>  {
             <Route exact path="/admin">
                 <div>
                 <Avatar src={user.avatarUrl} name={user.firstName + ' ' + user.lastName}size="105" round />  
-                
-           
                 
                 <h1>Welcome {user.firstName}</h1>
                     <div className={classes.root}>
@@ -177,25 +198,88 @@ const Admin = (props) =>  {
             </div>
             </Route>
             <Route path="/admin/profile">
+                <BreadcrumbWrapper>
+                    <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+                    <Link className={classes.link} to="/admin">
+                        <HomeIcon className={classes.icon} />
+                        Admin Home
+                    </Link>
+                    <Typography color="textPrimary">
+                        <AccountCircleIcon className={classes.icon} />
+                        Account Profile</Typography>
+                    </Breadcrumbs>
+                </BreadcrumbWrapper>
                 <AdminProfile user={user} setUser={setUser} setProfilePicture={setProfilePicture} profilePicture={profilePicture}/>
-                
             </Route>
+
             <Route path="/admin/team">
+                <BreadcrumbWrapper>
+                    <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+                    <Link className={classes.link} to="/admin">
+                        <HomeIcon className={classes.icon} />
+                        Admin Home
+                    </Link>
+                    <Typography color="textPrimary">
+                        <GroupIcon className={classes.icon} />
+                        Team Members
+                        </Typography>
+                    </Breadcrumbs>
+                </BreadcrumbWrapper>
                 <TeamList />
             </Route>
+
             <Route path="/admin/userlist">
+                <BreadcrumbWrapper>
+                    <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+                    <Link className={classes.link} to="/admin">
+                        <HomeIcon className={classes.icon} />
+                        Admin Home
+                    </Link>
+                    <Typography color="textPrimary">
+                        <AssignmentIcon className={classes.icon} />
+                        Documents and Forms
+                        </Typography>
+                    </Breadcrumbs>
+                </BreadcrumbWrapper>
                 <AdminForms user={user}/>
             </Route>
+
             <Route path="/admin/emergencycards">
+                <BreadcrumbWrapper>
+                    <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+                    <Link className={classes.link} to="/admin">
+                        <HomeIcon className={classes.icon} />
+                        Admin Home
+                    </Link>
+                    <Typography color="textPrimary">
+                        <RecentActorsIcon className={classes.icon} />
+                        Emergency Cards
+                        </Typography>
+                    </Breadcrumbs>
+                </BreadcrumbWrapper>
                 <h1 className="removePrint">Emergency Cards</h1>
                 <AdminEmergencyCards user={user}/>
             </Route>
+
             <Route path="/admin/rider/:id">
+                <BreadcrumbWrapper>
+                    <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+                    <Link className={classes.link} to="/admin">
+                        <HomeIcon className={classes.icon} />
+                        Admin Home
+                    </Link>
+                    <Link className={classes.link} to="/admin/team">
+                        <GroupIcon className={classes.icon} />
+                        Team Members
+                    </Link>
+                    <Typography color="textPrimary">
+                        <PersonIcon className={classes.icon} />
+                        Rider Profile
+                        </Typography>
+                    </Breadcrumbs>
+                </BreadcrumbWrapper>
                 <RiderCard />
             </Route>
-            
-            
-            
         </Switch>
         </>
     );
--- src/components/Admin/GetCardInfo.jsx
+++ src/components/Admin/GetCardInfo.jsx
@@ -1,6 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
-import { CardContent } from '@material-ui/core';
+import { useParams, Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { CardContent, Breadcrumbs, Typography } from '@material-ui/core';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import HomeIcon from '@material-ui/icons/Home';
+import GroupIcon from '@material-ui/icons/Group';
+import PersonIcon from '@material-ui/icons/Person';
 import styled from 'styled-components';
 
 const Card = styled.div`
@@ -35,10 +40,28 @@ const QRCode = styled.img`
     }
 `;
 
+const BreadcrumbWrapper = styled.div`
+    background-color: #FFBA00;
+    width: 350px;
+    border-radius: 0 7px 7px 0;
+`;
+
+const useStyles = makeStyles(() => ({
+    link: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    icon: {
+        marginRight: "5px",
+        width: 20,
+        height: 20,
+    },
+}));
+
 
 
 const GetCardInfo = (props) => {
-
+    const classes = useStyles();
     const [member, setMember] = useState('');
     const { id } = useParams();
 
@@ -59,6 +82,24 @@ const GetCardInfo = (props) => {
 
     return (
         <>
+            <BreadcrumbWrapper>
+                <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+                <Link className={classes.link} to="/admin">
+                    <HomeIcon className={classes.icon} />
+                    Admin Home
+                </Link>
+                <Link className={classes.link} to="/admin/team">
+                    <GroupIcon className={classes.icon} />
+                    Team Members
+                </Link>
+                <Typography color="textPrimary">
+                    <PersonIcon className={classes.icon} />
+                    Emergency Card
+                    </Typography>
+                </Breadcrumbs>
+            </BreadcrumbWrapper>
+
+
         {!!member ? (
 
 <Card key={member._id} variant="outlined">
